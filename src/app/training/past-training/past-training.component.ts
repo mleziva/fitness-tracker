4e3875c54dcb9053404f042751bbd89a7865644b
Austin Mleziva <austinmleziva@gmail.com>
2022-09-08 10:21:45 -0500

    adding datatable features

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
 import { MatTableDataSource } from '@angular/material/table'
+import { filter } from 'rxjs'
 import { Exercise } from '../exercise.model'
 import { TrainingService } from '../training.service'
 
@@ -8,12 +11,24 @@ import { TrainingService } from '../training.service'
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css'],
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state']
   dataSource = new MatTableDataSource<Exercise>()
+  @ViewChild(MatSort) sort!: MatSort
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+
   constructor(private trainingService: TrainingService) {}
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
+  }
 
   ngOnInit(): void {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises()
+  }
+  doFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase()
   }
 }
